273e07764d2a4efbb6e0938bed4093b23fc6f220
Kseniia Vengsar <122027680+KseniiaVengsar@users.noreply.github.com>
2023-03-22 08:22:28 +0000

    Update 1_ADDRESS.cpp

--- 1_ADDRESS/1_ADDRESS.cpp
+++ 1_ADDRESS/1_ADDRESS.cpp
@@ -7,9 +7,10 @@
 class address
 {
 public:
-    std::string get_output_address(std::string city_name, std::string street_name, int house_number, int apartment_number);
     address(std::string city_name, std::string street_name, int house_number, int apartment_number);
-    address() { int N, C, D; std::string A, B; }
+    address();
+    void mainFunc();
+    void get_output_address(std::string city_name_, std::string street_name_, int house_number_, int apartment_number_);
 
 private:
     std::string city_name;
@@ -18,23 +19,68 @@ private:
     int apartment_number;
 };
 
+address::address() { }
+
 address::address(std::string city_name_, std::string street_name_, int house_number_, int apartment_number_)
 {
     city_name = city_name_;
     street_name = street_name_;
     house_number = house_number_;
     apartment_number = apartment_number_;
-}
-//Создайте в вашем классе метод, который собирает строку для вывода в файл.
 
-std::string address::get_output_address(std::string city_name, std::string street_name, int house_number, int apartment_number)
+    get_output_address(city_name, street_name, house_number, apartment_number);
+}
+
+void address::mainFunc()
 {
-    //Формат вывода адреса: <название города>, <название улицы>, <номер дома>, <номер квартиры>.
-    return city_name + "," + " " + street_name + "," + " " + std::to_string(house_number) + "," + " " + std::to_string(apartment_number);
+    std::string city_name_;
+    std::string street_name_;
+    int house_number_;
+    int apartment_number_;
 
+    std::ifstream fin("in.txt");
+    std::ofstream fout("out.txt", std::ios::trunc);
+    std::cout << "info from in.txt:" << std::endl;
+
+    int N;
+    fin >> N;
+    fout << N << std::endl;
+    std::cout << N << std::endl;
+
+    address* mas = new address[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        fin >> mas[i].city_name;
+        fin >> mas[i].street_name;
+        fin >> mas[i].house_number;
+        fin >> mas[i].apartment_number;
+        
+    }
+
+    for (int i = N-1; i >= 0; i--)
+    {
+        fout << mas[i].city_name + "," + " ";
+        fout << mas[i].street_name + "," + " ";
+        fout << std::to_string(mas[i].house_number) + "," + " ";
+        fout << std::to_string(mas[i].apartment_number) << std::endl;
+
+        city_name_ = mas[i].city_name;
+        street_name_ = mas[i].street_name;
+        house_number_ = mas[i].house_number;
+        apartment_number_ = mas[i].apartment_number;
+
+        address(city_name_, street_name_, house_number_, apartment_number_);
+    }
 }
 
-int main() {
+void address::get_output_address(std::string city_name_, std::string street_name_, int house_number_, int apartment_number_)
+{ 
+    std::cout << city_name_ + "," + " " + street_name_ + "," + " " + std::to_string(house_number_) + "," + " " + std::to_string(apartment_number_) << std::endl;
+}
+
+int main()
+{
     setlocale(LC_ALL, "RU");
     address info;
 
@@ -46,62 +92,8 @@ int main() {
     }
     else if (fin.is_open())
     {
-        std::cout << "info from in.txt:" << std::endl;
-        int N, Hnumber, Anum, Hnumber_1, Anum_1, Hnumber_2, Anum_2;
-        std::string city, street, city_1, street_1, city_2, street_2;
-        fin >> N;
-        fin >> city;
-        fin >> street;
-        fin >> Hnumber;
-        fin >> Anum;
-
-        fin >> city_1;
-        fin >> street_1;
-        fin >> Hnumber_1;
-        fin >> Anum_1;
-
-        fin >> city_2;
-        fin >> street_2;
-        fin >> Hnumber_2;
-        fin >> Anum_2;
-        std::cout << N << std::endl;
-        //Для хранения всех адресов, экземпляров вашего класса, заведите динамический массив типа address* 
-        std::ofstream fout("out.txt", std::ios::trunc);
-        address* mas = new address[N];
-        fout << N << std::endl;
-        /////////////////////////////////////////////////////////////////////////////////////      
-        int i=0;
-
-        mas[i] = address(city_2, street_2, Hnumber_2, Anum_2);
-        fout << info.get_output_address(city_2, street_2, Hnumber_2, Anum_2) << std::endl;
-        std::cout << info.get_output_address(city_2, street_2, Hnumber_2, Anum_2) << std::endl;
-        ////////////////////////////////////////////////////////////////////////////////////////
-        mas[i] = address(city_1, street_1, Hnumber_1, Anum_1);
-        fout << info.get_output_address(city_1, street_1, Hnumber_1, Anum_1) << std::endl;
-        std::cout << info.get_output_address(city_1, street_1, Hnumber_1, Anum_1) << std::endl;
-
-        /////////////////////////////////////////////////////////////////////////////////////////    
-        mas[i] = address(city, street, Hnumber, Anum);
-        fout << info.get_output_address(city, street, Hnumber, Anum) << std::endl;
-        std::cout << info.get_output_address(city, street, Hnumber, Anum) << std::endl;
-
-        delete[]mas;
+        info.mainFunc();
     }
-
-    fin.close();
-
 
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
