c44a16fda979fe3ecb6a0f3e04b9162cfe05f97b
Kseniia Vengsar <122027680+KseniiaVengsar@users.noreply.github.com>
2023-03-31 21:22:10 +0100

    Update 1_ADDRESS.cpp

--- 1_ADDRESS/1_ADDRESS.cpp
+++ 1_ADDRESS/1_ADDRESS.cpp
@@ -1,3 +1,6 @@
+// ADDRESS_KV_2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -7,73 +10,54 @@
 class address
 {
 public:
-    address();
-    address(std::string city_name_, std::string street_name_, int house_number_, int apartment_number_);
-    void get_output_address();
+  
+
+   void setINFO(std::string k, std::string j, int u, int x) {
+        city_name = k;
+        street_name = j;
+        house_number = u;
+        apartment_number = x;
+    }
+
+
+   /* void set_city_name(std::string k) {
+        city_name = k;
+    }
+    void set_street_name(std::string j) {
+        street_name = j;
+    }
+    void set_house_number(int u) {
+        house_number = u;
+    }
+    void set_apart_number(int x) {
+        apartment_number = x;
+    }*/
+
+    std::string outAddress();
 
 private:
+
     std::string city_name;
     std::string street_name;
     int house_number;
     int apartment_number;
 };
 
-address::address() {}
-
-address::address(std::string city_name_, std::string street_name_, int house_number_, int apartment_number_) : city_name { city_name_ }, street_name { street_name_ }, house_number { house_number_ }, apartment_number{ apartment_number_ }
+std::string address::outAddress()
 {
-    //city_name = city_name_;
-    //street_name = street_name_;
-    //house_number = house_number_;
-    //apartment_number = apartment_number_;
-
-    std::cout << city_name + "," + " " + street_name + "," + " " + std::to_string(house_number) + "," + " " + std::to_string(apartment_number) << std::endl;
+ 
+    return city_name + ", " + street_name + ", " + std::to_string(house_number) + ", " + std::to_string(apartment_number);
 }
-
-void address::get_output_address()
-{
-    std::string city_name_;
-    std::string street_name_;
-    int house_number_;
-    int apartment_number_;
-
-    std::ifstream fin("in.txt");
-    std::ofstream fout("out.txt", std::ios::trunc);
-    std::cout << "info from in.txt:" << std::endl;
-
-    int N;
-    fin >> N;
-    fout << N << std::endl;
-    std::cout << N << std::endl;
-
-    address* mas = new address[N];
-
-    for (int i = 0; i < N; i++)
-    {
-        fin >> mas[i].city_name;
-        fin >> mas[i].street_name;
-        fin >> mas[i].house_number;
-        fin >> mas[i].apartment_number;
-    }
-
-    for (int i = N - 1; i >= 0; i--)
-    {
-        fout << mas[i].city_name + ", ";
-        fout << mas[i].street_name + ", ";
-        fout << std::to_string(mas[i].house_number) + ", ";
-        fout << std::to_string(mas[i].apartment_number) << std::endl;
-
-        address(mas[i].city_name, mas[i].street_name, mas[i].house_number, mas[i].apartment_number);    
-    }
-    delete[]mas;
-} 
 
 int main()
 {
     setlocale(LC_ALL, "RU");
     address info;
+    std::string a, b;
+    int c = 0; int d = 0;
 
     std::ifstream fin("in.txt");
+   
 
     if (fin.fail())
     {
@@ -81,9 +65,71 @@ int main()
     }
     else if (fin.is_open())
     {
-        info.get_output_address();
-    }
+        std::ofstream fout("out.txt", std::ios::trunc);
+        std::cout << "info from in.txt:" << std::endl;
+        int N;
+        fin >> N;
 
-    fin.close();
+        std::cout << N << std::endl;
+        fout << N << std::endl;
+
+        address* mas = new address[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            fin >> a;
+            fin >> b;
+            fin >> c;
+            fin >> d;
+
+            mas[i].setINFO(a, b, c, d);
+            mas[i].setINFO(a, b, c, d);
+            mas[i].setINFO(a, b, c, d);
+            mas[i].setINFO(a, b, c, d);
+
+            //fout << mas[i].outAddress() << std::endl;
+            //std::cout << mas[i].outAddress() << std::endl;
+        }
+
+
+
+      for (int i = N - 1; i >= 0; i--)
+        {
+          mas[i].setINFO(a, b, c, d);
+          mas[i].setINFO(a, b, c, d);
+          mas[i].setINFO(a, b, c, d);
+          mas[i].setINFO(a, b, c, d);
+
+             fout << mas[i].outAddress() << std::endl;
+             std::cout << mas[i].outAddress() << std::endl;
+        }
+
+        
+    }
     return 0;
+    
 }
+
+
+
+
+
+
+/*For getter it is simply to do. You declare parameter that will specify element's index of array.
+
+For example
+
+int get_element( size_t index ) { return ( a[index] ); }
+
+For setter you can specify two parameters: element's index and a new value
+
+void set_element( size_t index, int value ) { a[index] = value; }
+
+Also you can overload operator [] if your array is a part of a class definition.
+void setINFO(int i, std::string k, std::string j, int u, int x) {
+
+            mas[i] = k;
+            mas[i] = j;
+            mas[i] = u;
+            mas[i] = x;
+        }*/
